f1872c1736f8bb43431ca964b5c1b180852fa689
matkoodi <matkoodi@gmail.com>
2020-03-01 23:37:40 +0200

    WebSocket support for frontend

--- kps-frontend/src/App.js
+++ kps-frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { Client, Message } from '@stomp/stompjs';
+import SockJS from "sockjs-client"
+import { Stomp } from '@stomp/stompjs';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Radio from '@material-ui/core/Radio';
@@ -17,22 +18,62 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-function RadioButtonsGroup(props) {
+function Match(props) {
   const classes = useStyles();
   const [myHand, setMyHand] = React.useState('');
-  const [otherHand, setOtherHand] = React.useState('otherHand');
+  const [result, setResult] = React.useState({result: null, otherHand: ''});
+  const [connected, setConnected] = React.useState(false);
 
-  console.log(myHand);
   const handleHand = event => {
-    setMyHand(event.target.value);
+    const hand = event.target.value;
+    setMyHand(hand);
+    fetch(`http://localhost:8080/match/${props.matchId}/${props.playerId}/${hand}`, {method: 'POST'});
   };
 
-  var disabled = true;
-  return (
+  React.useEffect(() => {
+    if(connected) {
+      return;
+    }
+
+    const socket = new SockJS('http://localhost:8080/websocket');
+    const stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, function (frame) {
+      setConnected(true);
+      stompClient.subscribe('/topic/' + props.matchId, function(message) {
+        const dto = JSON.parse(message.body);
+
+        if(!dto.winner) {
+          var result = 'DRAW';
+        }
+        else if(dto.winner == props.playerId) {
+          var result = 'WIN';
+        }
+        else {
+          var result = 'LOSE';
+        }
+
+        setResult({
+          result: result,
+          hands: dto.hands
+        })
+      });
+    });
+  });
+
+  function getOtherPlayersHand(hands, myHand) {
+    if(!hands) {
+      return null;
+    }
+
+    return hands[0] != myHand ? hands[0] : hands[1];
+  }
+
+  return !connected ? null : (
     <div>
       <FormControl component="fieldset" className={classes.formControl}>
         <FormLabel component="legend">Me</FormLabel>
-        <RadioGroup aria-label="gender" name="player1" value={myHand} onChange={handleHand}>
+        <RadioGroup name="player1" value={myHand} onChange={handleHand}>
           <FormControlLabel disabled={myHand != ''} value="ROCK" control={<Radio />} label="Rock" />
           <FormControlLabel disabled={myHand != ''} value="PAPER" control={<Radio />} label="Paper" />
           <FormControlLabel disabled={myHand != ''} value="SCISSORS" control={<Radio />} label="Scissors" />
@@ -40,28 +81,66 @@ function RadioButtonsGroup(props) {
       </FormControl>
       <FormControl component="fieldset" className={classes.formControl}>
         <FormLabel component="legend">Other player</FormLabel>
-        <RadioGroup aria-label="gender" name="player1" value={otherHand}>
+        <RadioGroup name="player1" value={getOtherPlayersHand(result.hands, myHand)}>
           <FormControlLabel disabled value="ROCK" control={<Radio />} label="Rock" />
           <FormControlLabel disabled value="PAPER" control={<Radio />} label="Paper" />
           <FormControlLabel disabled value="SCISSORS" control={<Radio />} label="Scissors" />
         </RadioGroup>
       </FormControl>
+      {result.result ? <h1>{result.result}</h1> : null}
     </div>
   );
 }
 
+const matchIdRegexp = /^[a-zA-Z0-9]+$/;
+
 function App() {
   const [matchId, setMatchId] = React.useState('');
-  function keyPress(e) {
+  const [playerId, setPlayerId] = React.useState('');
+  const [error, setError] = React.useState('');
+  const [matchIdError, setMatchIdError] = React.useState('');
+
+  async function keyPress(e) {
     if(e.keyCode == 13) {
-      setMatchId(e.target.value);
+      let matchId = e.target.value;
+
+      if(!matchIdRegexp.test(matchId)) {
+        setMatchIdError('Allowed characters: A-Z, a-z, 0-9');
+        return;
+      }
+      else {
+        setMatchIdError('');
+      }
+
+      setMatchId(matchId);
+
+      let response = await fetch(`http://localhost:8080/match/${matchId}`, {method: 'POST'});
+
+      if(!response.ok) {
+        switch(response.status) {
+          case 403:
+            setError('The match is full.');
+            break;
+            case 400:
+              setError('Bad request. Illegal match ID?');
+              break;
+            default:
+              setError('Unknown error.')
+        }
+      }
+      else {
+        let playerId = await response.text();
+        setPlayerId(playerId);
+      }
     }
   }
+
   return (
     <div>
-      <TextField id="match-id" label="Match ID" disabled={matchId != ''} onKeyDown={keyPress}/>
+      <TextField error={matchIdError} id="match-id" label="Match ID" disabled={matchId != ''} onKeyDown={keyPress} helperText={matchIdError}/>
       <FormHelperText>Enter Match ID and press return to create or join a match</FormHelperText>
-      {matchId == '' ? null : <RadioButtonsGroup matchId={matchId}/>}
+      {playerId == '' ? null : <Match matchId={matchId} playerId={playerId}/>}
+      {error == '' ? null : <div>Error: {error}</div>}
     </div>
   );
 }
